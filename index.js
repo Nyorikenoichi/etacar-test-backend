70238e3bf8c53186b9ff32d8c604ee832bb1e8d1
Nyorikenoichi <maks.malsef@gmail.com>
2022-08-19 19:27:42 +0300

    configure GraphQL RootQuery with mockup data

--- index.js
+++ index.js
@@ -1,19 +1,52 @@
 const express = require("express");
-const graphql = require("graphql");
+const {GraphQLObjectType, GraphQLSchema, GraphQLString, GraphQLList} = require("graphql");
 const {graphqlHTTP} = require("express-graphql");
 const currenciesData = require("./MOCK_DATA.json");
 
 const app = express();
 const PORT = 6969;
 
-const RootQuery = "query"
-const Mutation = "mutation"
+const CurrencyType = new GraphQLObjectType({
+    name: "Currency",
+    fields: () => ({
+        id: { type: GraphQLString },
+        rank: { type: GraphQLString },
+        symbol: { type: GraphQLString },
+        name: { type: GraphQLString },
+        supply: { type: GraphQLString },
+        maxSupply: { type: GraphQLString },
+        marketCapUsd: { type: GraphQLString },
+        volumeUsd24Hr: { type: GraphQLString },
+        priceUsd: { type: GraphQLString },
+        changePercent24Hr: { type: GraphQLString },
+        vwap24Hr: { type: GraphQLString }
+    })
+})
 
-const schema = new graphql.GrapgQLSchema({query: RootQuery, mutation: Mutation});
+const RootQuery = new GraphQLObjectType({
+    name: "RootQueryType",
+    fields: {
+        getAllCurrencies: {
+            type: new GraphQLList(CurrencyType),
+            resolve() {
+                return currenciesData;
+            }
+        },
+        getCurrencyById: {
+            type: CurrencyType,
+            args: { id: { type: GraphQLString }},
+            resolve(parent, args) {
+                return currenciesData.find((item) => item.id === args.id);
+            }
+        }
+    }
+});
+
+const schema = new GraphQLSchema({query: RootQuery});
 
 app.use('/graphql', graphqlHTTP({
     schema,
-    graphql: true
+    graphiql: true
 }));
 
 app.listen(PORT, () => {
