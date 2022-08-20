d4f6b0e17ae947b24d8fed5e6c0f099397126aca
Nyorikenoichi <maks.malsef@gmail.com>
2022-08-20 18:30:18 +0300

    add cors middleware to fix cors error

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
 const express = require("express");
 const { graphqlHTTP } = require("express-graphql");
 const  schema = require("./schemas/index");
+const cors = require("cors");
 
 const app = express();
 const PORT = 6969;
 
-app.use('/graphql', graphqlHTTP({
+app.use(cors());
+
+app.use("/graphql", graphqlHTTP({
     schema,
     graphiql: true
 }));
 
 app.listen(process.env.PORT || PORT, () => {
-    console.log('server started');
+    console.log("server started");
 })
